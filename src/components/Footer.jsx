7b5bb893bebb1833aa61e605ef44b1035d83469a
AKASHWG <akashwihanga2005@gmail.com>
2025-03-01 13:05:54 +0000

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,39 +2,34 @@ import { FaDiscord, FaTwitter } from "react-icons/fa";
 
 const socialLinks = [
   { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://x.com/Raidox_Games", icon: <FaTwitter /> },
 ];
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-gradient-to-br from-violet-950 to-black py-4 text-white">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm font-light md:text-left font-bold">
-          ©Raidox 2024. All rights reserved
-        </p>
+      <footer className="w-screen bg-gradient-to-br from-violet-950 to-black py-4 text-white">
+        <div className="container  flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+          <p className="text-center text-sm  md:text-left font-light">
+            ©Raidox 2024. All rights reserved
+          </p>
 
-        <div className="flex justify-center gap-4  md:justify-start">
-          {socialLinks.map((link, index) => (
-            <a
-              key={index}
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white transition-transform hover:scale-125 duration-300 "
-            >
-              {link.icon}
-            </a>
-          ))}
+          <div className="absolute right-5 flex justify-end gap-4 pr-3">
+            {socialLinks.map((link, index) => (
+                <a
+                    key={index}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white transition-transform hover:scale-150 duration-300 "
+                >
+                  {link.icon}
+                </a>
+            ))}
+          </div>
+
+
         </div>
-
-        <a
-          href="#privacy-policy"
-          className="text-center text-sm font-light hover:underline md:text-right"
-        >
-          Privacy Policy
-        </a>
-      </div>
-    </footer>
+      </footer>
   );
 };
 
